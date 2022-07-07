b0d42d2dbeae1d625909b538b7ea7e59a49d5311
DjadjiT <djadji.traore75@gmail.com>
2022-07-08 00:57:02 +0200

    first commit with principal pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { BackOfficePageComponent } from './back-office-page/back-office-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { VetPageComponent } from './vet-page/vet-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+import { SearchVetPageComponent } from './search-vet-page/search-vet-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProfilePageComponent,
+    BackOfficePageComponent,
+    LoginPageComponent,
+    VetPageComponent,
+    SearchVetPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
+  exports:[
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
