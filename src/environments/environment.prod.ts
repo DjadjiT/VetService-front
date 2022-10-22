55cb811d741e7c73d539448e666d2517036ad910
DjadjiT <djadji.traore75@gmail.com>
2022-10-22 23:43:15 +0200

    upd file env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   proxy : "",
-  baseUrl: "Vetservicesbackeb-env.eba-enp6yzu3.eu-west-3.elasticbeanstalk.com",
+  baseUrl: "http://vetservicesbackeb-env.eba-enp6yzu3.eu-west-3.elasticbeanstalk.com/",
   subKey: "price_1LJDtCK6LFtOfBbROeMdH0Ot"
 };
